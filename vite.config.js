52b1481e7e8976e27ac22693f9322fd6e15fe755
koushub <you@example.com>
2024-05-29 08:24:52 +0530

    1

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,9 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import dotenv from 'dotenv';
-
-dotenv.config();
 
 export default defineConfig({
   plugins: [react()],
   server: {
-    port: process.env.PORT || 3000
+    port: parseInt(import.meta.env.VITE_PORT) || 3000
   }
 });
